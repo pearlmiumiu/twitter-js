bfc7edf0b8f760fab30bde28906f943cf9248e24
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2017-11-07 14:29:43 -0500

    update middleware

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ const express=require('express');
 const app=express();
 
 
+// app.use('/',function(req, res, next){
+// 	console.log("Get / ");
+// 	next();
+// }, function(req, res, next){
+// 	console.log('Get / is-anybody-in-there')
+// 	next();
+// },function(req, res, next){
+// 	console.log('Post /modernism')
+// 	next()
+// })
 
+app.use('/special/', function(req, res, next){
+	console.log('Get / 200')
+	next();
+})
+
+app.get('/special/', function(req, res, next){
+	res.send('getting special')
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -10,3 +28,18 @@ app.get('/news', (req, res) => res.send('hi there!'))
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
