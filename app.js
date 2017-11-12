08f2a7a5283db947fa2956528009b0ed47dad885
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2017-11-12 15:45:07 -0500

    tweets

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-const express=require('express');
-const app=express();
+'use strict';
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var makesRouter = require('./routes');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var bodyParser = require('body-parser');
+var socketio = require('socket.io');
+
+// templating boilerplate setup
+app.engine('html', nunjucks.render); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
++nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
+
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
 
 
-// app.use('/',function(req, res, next){
-// 	console.log("Get / ");
-// 	next();
-// }, function(req, res, next){
-// 	console.log('Get / is-anybody-in-there')
-// 	next();
-// },function(req, res, next){
-// 	console.log('Post /modernism')
-// 	next()
-// })
+// start the server
+var server = app.listen(1337, function(){
+  console.log('listening on port 1337');
+});
+var io = socketio.listen(server);
 
-app.use('/special/', function(req, res, next){
-	console.log('Get / 200')
-	next();
-})
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/special/', function(req, res, next){
-	res.send('getting special')
-})
+// modular routing that uses io inside it
+app.use('/', makesRouter(io));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/news', (req, res) => res.send('hi there!'))
-
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// // manually-written static file middleware
+// app.use(function(req, res, next){
+//   var mimeType = mime.lookup(req.path);
+//   fs.readFile('./public' + req.path, function(err, fileBuffer){
+//     if (err) return next();
+//     res.header('Content-Type', mimeType);
+//     res.send(fileBuffer);
+//   });
+// });
